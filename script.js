1048e78767549a7f68d33fe9ae9f1a3e6238c248
Deepak Kumar Saxena <deepzsenu@gmail.com>
2024-07-22 01:08:20 +0530

    updated rangilalls new version

---
+++ script.js
@@ -0,0 +1,25 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("rangilalls-header-slides");
+    let dots = document.getElementsByClassName("rangilalls-header-dot");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function currentSlide(n) {
+    slideIndex = n;
+    showSlides();
+}
